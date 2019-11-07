811c6146b6d016554ce81a85905db2e9f5ed08ac
Axel Rotter <axel_rotter@hotmail.com>
2019-11-07 17:03:15 +0100

    intwer commit

--- Godot Puzzle Pusher/Levels/Level.gd
+++ Godot Puzzle Pusher/Levels/Level.gd
@@ -13,6 +13,7 @@ onready var S_puzzle_piece = preload("res://Puzzle Pieces/S_Puzzle_Piece.tscn") 
 var S_puzzle_piece_array: Array
 var puzzle_piece_positions: PoolVector3Array
 
+
 func _ready():
 	SI_out_of_bounds.calculate_out_of_bounds_positions(self)
 	
@@ -43,20 +44,31 @@ func is_cell_vacant(this_world_pos=Vector3(), direction=Vector3()) -> Vector3:
 	var new_position
 	var target_grid_pos = world_to_map(this_world_pos) + direction
 	
+	#True if the player is trying to go out of bounds or a Puzzle piece bieng pushed out of bounds
+	var out_of_bounds = false
 	var puzzle_piece_collides = false
 	
-	for puzzle_piece in S_puzzle_piece_array:
-		if world_to_map(puzzle_piece.translation) == target_grid_pos:
-			#Check if this piece + direction will collide with another piece
-			for piece_position in puzzle_piece_positions:
-				if world_to_map(puzzle_piece.translation + direction) == world_to_map(piece_position):
-					puzzle_piece_collides = true
-			
-			if !puzzle_piece_collides:
-				puzzle_piece = puzzle_piece as KinematicBody
-				puzzle_piece.set_direction(direction)
-			else:
-				return this_world_pos
+	#Is the player trying to move out of bounds
+	if _is_moving_out_of_bounds(target_grid_pos) == false:
+		for puzzle_piece in S_puzzle_piece_array:
+			#Get Puzzle piece if it lies in the path of the target direction of the player
+			if world_to_map(puzzle_piece.translation) == target_grid_pos:
+				#Is the puzzle piece trying to move out of bounds
+				if _is_moving_out_of_bounds(world_to_map(puzzle_piece.translation)) == false:
+					#Check if this piece + direction will collide with another piece
+					for piece_position in puzzle_piece_positions:
+						if world_to_map(puzzle_piece.translation + direction) == world_to_map(piece_position):
+							puzzle_piece_collides = true
+						
+					
+					if !puzzle_piece_collides:
+						puzzle_piece = puzzle_piece as KinematicBody
+						puzzle_piece.set_direction(direction)
+					else:
+						return this_world_pos
+				
+	elif _is_moving_out_of_bounds(target_grid_pos) == true:
+		return this_world_pos
 	
 	new_position = map_to_world(target_grid_pos.x, target_grid_pos.y, target_grid_pos.z)
 	update_puzzle_positions()
@@ -67,4 +79,11 @@ func update_puzzle_positions():
 	for piece in S_puzzle_piece_array:
 		puzzle_piece_positions.append(piece.translation)
 		
-	
+
+func _is_moving_out_of_bounds(target_grid_position: Vector3) -> bool:
+	var target_grid_position_adapted = target_grid_position
+	target_grid_position_adapted.y -= 1;
+	for out_of_bound_position in SI_out_of_bounds.out_of_bounds_positions:
+		if out_of_bound_position == target_grid_position_adapted:
+			return true
+	return false
--- Godot Puzzle Pusher/Services/SI_out_of_bounds.gd
+++ Godot Puzzle Pusher/Services/SI_out_of_bounds.gd
@@ -4,8 +4,6 @@ var out_of_bounds_positions: PoolVector3Array
 
 var directions: PoolVector3Array
 
-
-
 func _ready():
 	directions.append(Vector3(1,0,0))
 	directions.append(Vector3(-1,0,0))
@@ -13,6 +11,8 @@ func _ready():
 	directions.append(Vector3(0,0,-1))
 
 func calculate_out_of_bounds_positions(gridMap: GridMap):
+	out_of_bounds_positions = PoolVector3Array()
+	
 	var used_cells = gridMap.get_used_cells() as PoolVector3Array
 	
 	var is_cell_taken = false
@@ -31,26 +31,46 @@ func calculate_out_of_bounds_positions(gridMap: GridMap):
 			if cell_taken == false:
 				out_of_bounds_positions.append(neighbour_direction)
 		
-		remove_double_entries()
+	_remove_double_entries()
+	_add_out_of_bounds_top_and_bottom()
 
 
-func remove_double_entries():
+func _remove_double_entries():
 	#Delete double entries in out of bounds
 	if out_of_bounds_positions.size() < 2:
 		return []
 
 	var seen = {}
 	seen[out_of_bounds_positions[0]] = true
-	var duplicate_indexes = []
+	var duplicate_positions = PoolVector3Array()
 	
 	for i in range(1, out_of_bounds_positions.size()):
 		var v = out_of_bounds_positions[i]
 		if seen.has(v):
 			# Duplicate!
-			duplicate_indexes.append(i)
+			duplicate_positions.append(out_of_bounds_positions[i])
 		else:
 			seen[v] = true
 	
-	if duplicate_indexes.size() > 0:
-		for double in duplicate_indexes:
-			out_of_bounds_positions.remove(double)
+	print(duplicate_positions)
+	
+	
+	
+#	if duplicate_indexes.size() > 0:
+#		for double in duplicate_indexes:
+#			out_of_bounds_positions.remove(double)
+
+func _add_out_of_bounds_top_and_bottom():
+	var top_bottom_positions_to_append = PoolVector3Array()
+	
+	for position in out_of_bounds_positions:
+		var top_piece = position as Vector3
+		top_piece.y += 1
+		var bottom_piece = position as Vector3
+		bottom_piece.y -= 1
+		
+		top_bottom_positions_to_append.append(top_piece)
+		top_bottom_positions_to_append.append(bottom_piece)
+	
+	for position in top_bottom_positions_to_append:
+		out_of_bounds_positions.append(position)
